a45dadb5e17ef8efb695c209f1b089cd5ea5f4d3
Michael Cavalea <callmecavs@gmail.com>
2015-06-19 18:06:32 -0400

    outset

---
+++ src/scripts/ready.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// CLASS
+
+class Outset {
+  constructor() {
+    document.addEventListener('DOMContentLoaded', this.create.bind(this), false);
+  }
+
+  create() {
+
+  }
+}
+
+// INSTANCE
+
+var outset = new Outset();
