dc9c67fa666b4c8676daecdde97db440beef7131
Michael Cavalea <callmecavs@gmail.com>
2015-06-19 18:34:00 -0400

    add 'add' method

--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function Emitter() {
+  this.handlers = {};
+}
+
+Emitter.prototype.add = function(eventName, eventHandler) {
+  this.handlers[eventName] = this.handlers[eventName] || [];
+  this.handlers[eventName].push(eventHandler);
+
+  return this;
+};
