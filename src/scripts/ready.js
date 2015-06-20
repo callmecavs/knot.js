4d5dc23a14d5d834c57bceb1615fe7c30a7f7247
Michael Cavalea <callmecavs@gmail.com>
2015-06-19 23:07:39 -0400

    add 'remove' method

--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -11,6 +11,13 @@ Emitter.prototype.add = function(eventName, eventHandler) {
   return this;
 };
 
+Emitter.prototype.remove = function(eventName, eventHandler) {
+  var index = this.handlers[eventName].indexOf(eventHandler);
+  this.handlers[eventName].splice(index, 1);
+
+  return this;
+};
+
 Emitter.prototype.removeAll = function(eventName) {
   delete this.handlers[eventName];
 
